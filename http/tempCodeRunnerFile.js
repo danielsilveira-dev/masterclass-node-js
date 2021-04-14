6af8abcd4b6c097870b16b17bb0514796edeca24
Daniel Silveira <danielsilveira.contato@gmail.com>
2021-04-14 10:00:52 -0300

    consu

--- http/tempCodeRunnerFile.js
+++ http/tempCodeRunnerFile.js
@@ -1,16 +1,14 @@
 const http = require('http')
-const fs = require('fs')
-const path = require('path')
-
+const fs = require('fs') // lida com os arquivos do sistema
+const path = require('path') // ações referentes às pastas
 
 http.createServer((req, res) => {
-
-    const file = req.url === '/' ? 'index.html' : req.url
+    const file = req.url === '/' ? 'index.html' : req.url // Se index igual a '/'(raiz) então exibe o arquivo index.html senão, exibe url todo
     const filePath = path.join(__dirname, 'public', file)
     const extname = path.extname(filePath)
 
     const allowedFileTypes = ['.html', '.css', '.js']
-    const allowed = allowedFileTypes.find(item => item == extname)
+    const allowed = allowedFileTypes.find(item => item == extname )
 
     if(!allowed) return
 
